912af58f18223d64b82107bf12099df06130b4c5
syed hadi alvi <hadi.alvi@devsinc.com>
2024-02-12 05:31:40 -0500

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,28 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
